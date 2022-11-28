ee869ae10dbf29e24e6379b883b1f0f6493b129f
Y99dr451l <matthias@kockisch.eu>
2022-11-29 00:59:30 +0100

    path following
    /!\ index out of range error

--- control.py
+++ control.py
@@ -1,56 +1,88 @@
-import math
-import RepeatedTimer
+import math, time
 from tdmclient import ClientAsync, aw
+from threading import Timer
+from RepeatedTimer import RepeatedTimer
 
-ODOMETRY_INTERVAL = 0.1
-WHEEL_DISTANCE = 95
-MMS_SPEED = 0.325 # to get mm/s from speed; thymio moves 65mm in 2s at 100 speed
+SPEED_TO_MMS = 0.32
+STANDARD_SPEED = 200 # 32cm/5s
+WHEEL_DIST = 95 # mm
+DIST_TOL = 10 # mm
+ANGLE_TOL = 0.1 # rad
+MOVE_INTERVAL = 0.15 # s
+ODOMETRY_INTERVAL = 0.025 # s
 
 class ThymioControl:
-    def __init__(self, position=(0,0), angle=0):
+    def __init__(self):
         self.client = ClientAsync()
         self.node = aw(self.client.wait_for_node())
         aw(self.node.lock())
-        # print(self.node)
-        self.position = position
-        self.angle = angle
-        self.speed_target = (0,0)
-        self.odometry_timer = RepeatedTimer.RepeatedTimer(ODOMETRY_INTERVAL, self.estimate_position)
+        self.position = (0,0)
+        self.angle = 0
+        # path following
+        self.moving = False
+        self.path = [(100,100), (0,0)]
+        self.path_index = 0
+        # odometry
+        self.odometry_time = None
+        self.speed_target = [0,0]
+        self.move_timer = RepeatedTimer(MOVE_INTERVAL, self.move_to_goal)
+        self.odometry_timer = RepeatedTimer(ODOMETRY_INTERVAL, self.estimate_position)
         self.odometry_timer.start()
 
-    def __del__(self):
-        self.unlock()
-        self.odometry_timer.cancel()
-        self.client.close()
-
-    def unlock(self):
-        aw(self.node.unlock())
-
-    def sleep(self, time): # might be better than time.sleep()? it's used in the library doc
-        aw(self.client.sleep(time))
-
-    def move(self, l_speed, r_speed=None): # speed in mm/s
+    def move(self, l_speed, r_speed=None):
         if r_speed is None: r_speed = l_speed
-        aw(self.node.set_variables({"motor.left.target": [(int)(l_speed/MMS_SPEED)],"motor.right.target": [(int)(r_speed/MMS_SPEED)]}))
-        # self.node.v.motor.left.target = l_speed/MMS_SPEED
-        # self.node.v.motor.right.target = r_speed/MMS_SPEED
-        # self.node.flush() # moves the changed variables to the thymio; probably slower than set_variables
-        self.speed_target = (l_speed, r_speed)
-        # print(self.speed_target)
-    
+        aw(self.node.set_variables({"motor.left.target": [(int)(l_speed)],"motor.right.target": [(int)(r_speed)]}))
+        self.speed_target = [l_speed, r_speed]
+
     # https://ocw.mit.edu/courses/6-186-mobile-autonomous-systems-laboratory-january-iap-2005/764fafce112bed6482c61f1593bd0977_odomtutorial.pdf
     def estimate_position(self):
-        aw(self.node.wait_for_variables({"motor.left.speed", "motor.right.speed"}))
-        dx = ODOMETRY_INTERVAL*MMS_SPEED*self.node.v.motor.left.speed
-        dy = ODOMETRY_INTERVAL*MMS_SPEED*self.node.v.motor.right.speed
-        # dx = ODOMETRY_INTERVAL*self.speed_target[0] # in case the readings are weird or the conversion value is wrong
-        # dy = ODOMETRY_INTERVAL*self.speed_target[1]
-        da = (dx - dy)/WHEEL_DISTANCE
+        if self.odometry_time is None:
+            self.odometry_time = time.time()
+            return
+        interval = time.time() - self.odometry_time
+        self.odometry_time = time.time()
+        dx = interval*SPEED_TO_MMS*self.speed_target[0] # reading the bot speeds takes frgn 100ms
+        dy = interval*SPEED_TO_MMS*self.speed_target[1] # so does writing to them, thus a remote PID is practically impossible
+        da = (dy - dx)/WHEEL_DIST # and a local PID would be hell because the thymio doesn't know floats
         dc = (dx + dy)/2
         self.position = (self.position[0] + dc*math.cos(self.angle), self.position[1] + dc*math.sin(self.angle))
-        self.angle = self.angle + da
-        print(self.position, self.angle)
+        self.angle = (self.angle + da) % (2*math.pi)
+        if not self.moving: print(self.position, self.angle*180/math.pi)
 
-    def correct_position(self, position, angle): # correction from the kalman filter
-        self.position = position
-        self.angle = angle
+    def move_to_goal(self):
+        if self.moving: return
+        goal = self.path[self.path_index]
+        dist = math.sqrt((goal[0]-self.position[0])**2 + (goal[1]-self.position[1])**2)
+        if dist > DIST_TOL:
+            angle = (math.atan2(goal[1]-self.position[1], goal[0]-self.position[0]) - self.angle + math.pi) % (2*math.pi) - math.pi
+            if abs(angle) > ANGLE_TOL:
+                print("turning" + str(angle))
+                direction = 1 if angle > 0 else -1
+                t = abs(angle)*WHEEL_DIST / (2*STANDARD_SPEED*SPEED_TO_MMS) - 0.1
+                self.timed_move(-direction*STANDARD_SPEED, direction*STANDARD_SPEED, t)
+            else:
+                print("moving" + str(dist))
+                t = dist / (STANDARD_SPEED*SPEED_TO_MMS) - 0.1
+                self.timed_move(STANDARD_SPEED, STANDARD_SPEED, t)
+        else:
+            self.path_index += 1
+
+    def stop(self):
+        self.move(0)
+        self.moving = False
+
+    def timed_move(self, l_speed, r_speed, time):
+        # if not self.moving: 
+        self.moving = True
+        stop_timer = Timer(time, self.stop)
+        self.move(l_speed, r_speed)
+        stop_timer.start()
+        # else : 
+        #     x = Timer(1, self.timed_move, [l_speed, r_speed, time])
+        #     x.start()
+    
+    def __del__(self):
+        self.move_timer.stop()
+        self.odometry_timer.stop()
+        self.stop()
+        self.client.close()
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from control import ThymioControl
 import time
 
+# PROX_THRESHOLD = 2000 # ~4.5cm
+
+# init path, position etc.
+# while not at goal:
+#   path following
+
+# call sensors every 0.1? seconds
+# if np.count_nonzero(thymio.get_prox() > PROX_THRESHOLD) > 0:
+#     turn off repeated timer
+#     while sensors detect something or not on path:
+#         if obstacle: avoid
+#         else: go to path
+#     turn on repeated timer
+
 thymio = ThymioControl()
-thymio.move(100)
-time.sleep(2)
-thymio.move(0,0)
-thymio.unlock()
+thymio.path = [(100,100), (0,0)]
+thymio.move_timer.start()
+
+# del(thymio)
