ca2c23b59fb20c33b43d89d4912572d000ce4c13
Y99dr451l <matthias@kockisch.eu>
2022-12-09 20:22:32 +0100

    fixed odometry crash (self.x format)

--- Kalman.py
+++ Kalman.py
@@ -13,7 +13,7 @@ class Kalman:
         self.dt = None
         self.prev_time = None
         # state
-        self.x = np.zeros((3,1)) # state
+        self.x = np.zeros((3)) # state
         self.P = 1000*np.ones((3,3)) # state covariance
         # process noise
         self.R = np.diag([NOISE_POS_XY, NOISE_POS_XY, NOISE_POS_THETA**2/6])
@@ -32,16 +32,17 @@ class Kalman:
         if self.prev_time is None: # initialisation
             self.prev_time = time.time()
             return
+        u = np.array(u)
         self.dt = time.time() - self.prev_time
         self.prev_time = time.time()
         # https://ocw.mit.edu/courses/6-186-mobile-autonomous-systems-laboratory-january-iap-2005/764fafce112bed6482c61f1593bd0977_odomtutorial.pdf
-        (dx, dy) = self.dt*SPEED_TO_MMS*np.array(u) # left and right displacements [mm]
+        (dx, dy) = self.dt*SPEED_TO_MMS*u # left and right displacements [mm]
         da = -(dy - dx)/WHEEL_DIST # rotation angle [rad]
         dc = (dx + dy)/2 # center displacement [mm]
-        (vx, vy) = SPEED_TO_MMS*np.array(u) # left and right wheel speeds [mm/s]
+        (vx, vy) = SPEED_TO_MMS*u # left and right wheel speeds [mm/s]
         vt = (vx + vy)/2 # translation speed [mm/s]
         vr = -(vy - vx)/WHEEL_DIST # rotation speed [rad/s]
-        print("angle is ", self.x[2])
+        #print("angle is ", self.x[2])
         sin = math.sin(self.x[2])
         cos = math.cos(self.x[2])
         # state propagation
@@ -49,20 +50,20 @@ class Kalman:
         self.x[1] = self.x[1] + dc*sin
         self.x[2] = (self.x[2] + da) % (2*math.pi)
         # transition function (state propagation matrix)
-        A = np.array([[1, 0, -self.dt*vt*sin - 0.5*self.dt**2*vt*vr*cos],
-                      [0, 1,  self.dt*vt*cos - 0.5*self.dt**2*vt*vr*sin],
+        A = np.array([[1, 0, -self.dt*vt*sin],
+                      [0, 1,  self.dt*vt*cos],
                       [0, 0, 1]])
         # input transition matrix
-        L = np.array([[self.dt*vx/2*cos - ((self.dt**2)/(2*WHEEL_DIST))*vx**2*sin, -self.dt*vy/2*cos + ((self.dt**2)/(2*WHEEL_DIST))*vy**2*sin],
-                      [self.dt*vx/2*cos + ((self.dt**2)/(2*WHEEL_DIST))*vx**2*cos, -self.dt*vx/2*sin - ((self.dt**2)/(2*WHEEL_DIST))*vx**2*cos],
-                      [self.dt*vx/WHEEL_DIST, -self.dt*vy/WHEEL_DIST]])           
+        L = np.array([[self.dt*vx/2*cos, -self.dt*vy/2*cos],
+                      [self.dt*vx/2*cos, -self.dt*vx/2*sin],
+                      [self.dt*vx/WHEEL_DIST, -self.dt*vy/WHEEL_DIST]])
         # state covariance propagation
         self.P = A@self.P@A.T + L@self.Q@L.T
     
     def state_correct(self, z):
         z = np.reshape(z, (3,1))
         K = self.P@self.H.T@np.linalg.inv(self.H@self.P@self.H.T + self.R) # kalman gain
-        self.x = self.x + K@(z - self.H@self.x) # state
+        self.x = (self.x.T + K@(z - self.H@self.x.T)).T # state
         self.P = (np.eye(3) - K@self.H)@self.P # state covariance
 
 if __name__ == "__main__":
