85dadd733e46bf8753ca8347536c6f402c306cee
Y99dr451l <matthias@kockisch.eu>
2022-12-10 20:40:36 +0100

    reset tolerances

--- constants.py
+++ constants.py
@@ -6,8 +6,8 @@ MOVE_INTERVAL = 0.21 # s # leave time for get_prox() and move() (100ms each)
 ODOMETRY_INTERVAL = 0.025 # s
 # MOVEMENT
 STANDARD_SPEED = 200 # 32cm/5s
-DIST_TOL = 20 # mm
-ANGLE_TOL = 0.4 # rad
+DIST_TOL = 10 # mm
+ANGLE_TOL = 0.1 # rad
 MAX_TIME = 0.3
 TIME_FACTOR = 0.75
 # OBSTACLE AVOIDANCE
@@ -25,6 +25,3 @@ NOISE_POS_XY = 0.64         # [mm2]
 NOISE_POS_THETA = 2         # [rad^2]
 NOISE_MEASURE_XY = 0.8      # [mm2]
 NOISE_MEASURE_THETA = 0.5     # [rad^2]
-
-#PATH FINDING
-angle_tolerance = 2*math.pi/180
