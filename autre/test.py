50384f6bf232b8e2a9b4ccedd35d6ac0703f22c7
Antoine Escoyez <escoyez.antoine@outlook.com>
2022-12-11 16:51:45 +0100

    cleaned repo

--- autre/test.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import matplotlib.pyplot as plt
-
-camera_device = 1
-cap = cv.VideoCapture(camera_device)
-print(cap.isOpened())
-success, frame = cap.read()
-plt.imshow(frame)
-plt.show()
